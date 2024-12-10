4927386e28c6110642852c31d19e94c0168e216c
MamenAseli <147691861+MamenHD@users.noreply.github.com>
2024-12-10 23:15:31 +0700

    Create main3.js

---
+++ main3.js
@@ -0,0 +1,9 @@
+// Mengubah nilai di properti Object
+
+const account = {
+    balance: 1000,
+    debt: 10,
+  };
+  
+  account.balance = 2000; // Bisa diubah angka awalnya menggunakan sistem objek
+  console.log(account.balance); // Output: 2000
