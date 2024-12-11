00a7ea13177baa1542bf1a2d825a26fb2c84c671
MamenAseli <147691861+MamenHD@users.noreply.github.com>
2024-12-11 22:53:26 +0700

    Create main18.js

---
+++ main18.js
@@ -0,0 +1,8 @@
+// Array dalam spread oprator
+
+const arry1 = ['MamenHD'];
+const arry2 = ['Indonseia', 'Ngoding'];
+
+const arraybaru = [...arry1, ...arry2]; // Penyatuan sebuah function
+
+console.log(arraybaru); // Output yang akan keluar ['MamenHD', 'Indonseia', 'Ngoding']
